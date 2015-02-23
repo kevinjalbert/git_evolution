24f6a505625d0bd74da825781ff591efc675a995
Kevin Jalbert <kevin.j.jalbert@gmail.com>
2015-02-23 00:24:30 -0500

    Add essential gems/raketasks and spec coverage

--- Gemfile
+++ Gemfile
@@ -2,3 +2,14 @@ source 'https://rubygems.org'
 
 # Specify your gem's dependencies in git_evolution.gemspec
 gemspec
+
+group :test do
+  gem 'simplecov'
+  gem 'coveralls', require: false
+  gem 'rspec'
+end
+
+group :development do
+  gem 'pry-byebug'
+  gem 'rubocop'
+end
---
+++ spec/support/coverage.rb
@@ -0,0 +1,16 @@
+begin
+  if ENV['CI']
+    require 'coveralls'
+    Coveralls.wear!
+  elsif ENV['COVERAGE']
+    require 'simplecov'
+  end
+
+  if ENV['CI'] || ENV['COVERAGE']
+    SimpleCov.start do
+      add_filter '/spec/'
+    end
+  end
+rescue LoadError => e
+  warn(e)
+end
