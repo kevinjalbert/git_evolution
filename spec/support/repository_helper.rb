9fac218f00da761af85979486a588f07a7319c6a
Kevin Jalbert <kevin.j.jalbert@gmail.com>
2015-02-28 17:43:56 -0500

    Add create_commit and add_to_index methods to RepositoryHelper
    #add_to_index provides a way to add files to the repository's index
    
    #create_commit creates a commit off the state of the repository's index
    
    The flow of using the RepositoryHelper is:
    
     create_repository
    
     add_to_index  // This can happen many times per commit
     create_commit // Add many commits by repeating #add_to_index and this
    
     delete_repository

--- spec/support/repository_helper.rb
+++ spec/support/repository_helper.rb
@@ -3,7 +3,7 @@ module RepositoryHelper
 
   def create_repository
     @tmp_git_dir = Dir.mktmpdir
-    Rugged::Repository.init_at(@tmp_git_dir)
+    @repo = Rugged::Repository.init_at(@tmp_git_dir)
   end
 
   def delete_repository
@@ -12,5 +12,26 @@ module RepositoryHelper
 
   def repository_dir
     File.realpath(@tmp_git_dir) + '/'
+  end
+
+  def add_to_index(file_name, blob_content)
+    object_id = @repo.write(blob_content, :blob)
+    @repo.index.add(path: file_name, oid: object_id, mode: 0100644)
+  end
+
+  def create_commit(author_name, author_email, time, subject, body = nil)
+    author = { email: author_email, name: author_name, time: time }
+
+    tree = @repo.index.write_tree(@repo)
+
+    Rugged::Commit.create(@repo,
+                          author: author,
+                          message: "#{subject}\n\n#{body}".strip,
+                          committer: author,
+                          parents: @repo.empty? ? [] : [@repo.head.target].compact,
+                          tree: tree,
+                          update_ref: 'HEAD')
+
+    @repo.checkout('master', {strategy: [:force]})
   end
 end
