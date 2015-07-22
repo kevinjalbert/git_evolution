7e18fc9abb6325c5d8dc541ac0f6403d2d5bc9a4
Kevin Jalbert <kevin.j.jalbert@gmail.com>
2015-07-22 07:47:36 -0400

    Refactor Commit class to break the parsing logic into separate methods
    Store the raw_commit on the Commit object

--- lib/git_evolution/commit.rb
+++ lib/git_evolution/commit.rb
@@ -1,12 +1,22 @@
 module GitEvolution
   class Commit
-    attr_reader :sha, :author, :date, :subject, :body, :additions, :deletions
+    attr_reader :raw_commit, :sha, :author, :date, :subject, :body, :additions, :deletions
 
     def initialize(raw_commit)
+      @raw_commit = raw_commit
+
+      parse_meta_data!
+      parse_body_data!
+      parse_diff_data!
+    end
+
+    def parse_meta_data!
       @sha = raw_commit.scan(/^commit\s+(.*?)$/).flatten.first.strip
       @author = raw_commit.scan(/^Author:\s+(.*?)$/).flatten.first.strip
       @date= raw_commit.scan(/^Date:\s+(.*?)$/).flatten.first.strip
+    end
 
+    def parse_body_data!
       raw_body_lines = (raw_commit + "\n\u0000").scan(/^Date:.*?$(.*?)^[diff|\u0000]/m).flatten.first.strip.split("\n")
       @subject = raw_body_lines.first.strip
 
@@ -14,8 +24,11 @@ module GitEvolution
         @body = raw_body_lines[1..-1].map { |line| line.gsub(/^\s+/, '') }.join("\n")
         @body.sub!(/\n+/, '') if @body.start_with?("\n")
       end
+    end
 
+    def parse_diff_data!
       raw_diff_lines = raw_commit.scan(/^@@.*?$(.*)?/m).flatten.first
+
       if raw_diff_lines
         raw_diff_lines = raw_diff_lines.strip.split("\n")
         @additions = raw_diff_lines.count { |line| line.start_with?('+') }
